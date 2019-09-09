4493fd56a0fa0a2e990e3889c43dda528ea5e7e3
yan zheng <moodcal@gmail.com>
2019-09-09 17:41:59 +0800

    preview in fullscreen

--- matisse/src/main/java/com/zhihu/matisse/internal/ui/BasePreviewActivity.java
+++ matisse/src/main/java/com/zhihu/matisse/internal/ui/BasePreviewActivity.java
@@ -83,7 +83,7 @@ public abstract class BasePreviewActivity extends AppCompatActivity implements V
         }
         setContentView(R.layout.activity_media_preview);
         if (Platform.hasKitKat()) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         }
 
         mSpec = SelectionSpec.getInstance();
